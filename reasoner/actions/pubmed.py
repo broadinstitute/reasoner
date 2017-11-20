4819260b40feb115cd113ac99be35ce9a2cfb6e6
mwawer <mwawer@arcor.de>
2017-11-20 17:04:12 -0500

    fix MeSH path again

--- reasoner/actions/pubmed.py
+++ reasoner/actions/pubmed.py
@@ -5,11 +5,11 @@ import xmltodict
 import pandas as pd
 import dateutil.parser
 
-class PubmedQuery(Action):
+class PubmedQuery(Action, mesh_path = './data/MeSH_hierarchy.txt'):
   def __init__(self, precondition_entities, effect_entities):
     super().__init__(precondition_entities, effect_entities)
     self.url_prefix = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
-    self.mesh_terms = pd.read_table('../../data/MeSH_hierarchy.txt')
+    self.mesh_terms = pd.read_table(mesh_path)
     
   def get_request(self, url):
     with urllib.request.urlopen(url) as response:
