6439a356dc9b8dc7cb4aa2762009558b57a5fb9e
mwawer <mwawer@arcor.de>
2017-11-22 20:00:07 -0500

    add list of terminal nodes to action

--- reasoner/actions/action.py
+++ reasoner/actions/action.py
@@ -1,3 +1,4 @@
+from collections import Counter
 
 class Action:
     def __init__(self, precondition, effect):
@@ -6,7 +7,9 @@ class Action:
         (self.effect_terms, self.effect_constraints) = self.__parse_effect(effect)
         (self.precondition_entities, self.precondition_bindings, self.precondition_connections) = self.__extract_entities(self.precondition)
         (self.effect_entities, self.effect_bindings, self.effect_connections) = self.__extract_entities(self.effect_terms) 
-
+        ecounter = Counter(x for t in self.effect_connections for x in t)
+        self.terminal_effect_entities = [k for k, v in ecounter.items() if v == 1 and not k in self.precondition_entities]
+        
     def __parse_effect(self, effect):
         parsed_effect = list()
         parsed_constraints = list()
@@ -22,17 +25,17 @@ class Action:
     def __extract_entities(self, term_list):
         entity_set = set()
         entity_bound = set()
-        connections = list()
+        connections = set()
         for term in term_list:
             if 'connected(' in term:
                 entities = [x.strip() for x in term[10:-1].split(',')]
                 entity_set.update(entities)
-                connections.append(tuple(entities))
+                connections.add(tuple(entities))
             elif 'bound(' in term:
                 entity = term[6:-1]
                 entity_set.add(entity)
                 entity_bound.add(entity)
-        return(list(entity_set), list(entity_bound), connections)
+        return(list(entity_set), list(entity_bound), list(connections))
     
     def execute(self, input):
         assert len(input) == len(self.precondition_entities)
