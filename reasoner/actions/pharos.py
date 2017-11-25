7c11daffd4642311772de04b6f05f4ad44605c4c
vdancik <vdancik@broadinstitute.org>
2017-11-25 16:17:41 -0500

    add Pharos target-to-disease action

--- reasoner/actions/pharos.py
+++ reasoner/actions/pharos.py
@@ -61,3 +61,52 @@ class PharosDrugToTarget(JsonApiAction):
             activity_value = self.find_property(activity_term,link['properties'])['numval']
 
         return [{'node':{'name':target, 'id':link['refid'], 'authority': 'Pharos:Target', 'URI':link['href']},'edge':{'p'+activity_term: activity_value}}]
+
+
+class PharosTargetToDisease(JsonApiAction):
+
+    def __init__(self):
+        super().__init__(['bound(Target)'],['bound(Pathway)', 'bound(Cell)', 'bound(Symptom)', 'connected(Target, Pathway)',  'connected(Target, Pathway) and connected(Pathway, Cell)',  'connected(Target, Pathway) and connected(Pathway, Cell) and connected(Cell, Symptom) and connected(Symptom, Disease)'])
+
+
+    def execute(self, query):
+        target = query['Target']
+        response = self.parse_request('https://pharos.nih.gov/idg/api/v1/targets/search?q='+quote(target))
+        content = response['content']
+        if len(content) != 1:
+            print("WARNING: Target not unique: "+target)
+            return []
+        links_url = content[0]['_links']['href']
+        print(links_url)
+        disease_list = []
+        links = self.parse_request(links_url)
+        for link in links:
+            if link['kind'] == 'ix.idg.models.Disease':
+                disease = self.link_to_disease(link)
+                if disease != None:
+                    disease_list.append(disease)
+        return disease_list
+
+
+    def link_to_disease(self, link):
+        properties = link['properties']
+        disease_name = self.get_property(properties,'IDG Disease')
+        if disease_name == None:
+            return None
+        disease_node = {'name': disease_name}
+        source = self.get_property(properties,'Data Source')
+        if source != None:
+            disease_node['source']=source
+        disease_edge = {}
+        for prop in properties:
+            if 'label' in prop and 'numval' in prop:
+                disease_edge[prop['label']]=prop['numval']
+
+        return {'Pathway':[], 'Cell':[], 'Symptom':[], 'Disease':[{'node': disease_node,'edge':disease_edge}]}
+
+
+    def get_property(seld, properties,property):
+        for prop in properties:
+            if prop['label'] == property:
+                return prop['term']
+        return None
