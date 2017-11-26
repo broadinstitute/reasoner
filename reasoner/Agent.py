e8d64bcefc5d65adc2675f9013cd7d991e7b5f39
mwawer <mwawer@arcor.de>
2017-11-26 18:11:47 -0500

    move actions from Agent to KM and document both

--- reasoner/Agent.py
+++ reasoner/Agent.py
@@ -9,14 +9,30 @@ from .KnowledgeMap import KnowledgeMap
 from .Blackboard import Blackboard, QueryBuilder
 from .QueryParser import QueryParser
 from .ConnectionPGM import ConnectionPGM
-
-from .actions.eutils import *
-from .actions.sparql import *
-from .actions.pharos import *
-from .actions.file_actions import *
+from .actions.eutils import PubmedEdgeStats
 
 class Agent:
-    def __init__(self, question, discount = 0.4, state_action_tuple=None):
+    """
+    Generate a reasoning agent to answer a provided question.
+    
+    The agent will attempt to answer an input question in four steps:
+    1. parse the input question using natural-language processing
+    2. create a plan to acquire knowledge
+    3. execute and, if necessary, iteratively adapt the plan
+    4. analyze the acquired knowledge and return an answer if one was found.
+
+    Parameters
+    ----------
+    
+    question : str
+       A question the agent should answer, formulated in English.
+       
+    discount : float
+       Discount value used by the MDP value iteration algorithm to find
+       a knowledge acquisiton plan. Must be in range [0,1].
+
+    """
+    def __init__(self, question, discount = 0.4):
         self.parser = QueryParser()
         self.blackboard = Blackboard()
         self.discount = discount
@@ -24,12 +40,14 @@ class Agent:
         if len(query) == 0:
             print('Query could not be parsed.')
             return None
-
-        if state_action_tuple is not None:
-          (action_list, state_vars, goal_state) = state_action_tuple
-        else:
-          (action_list, state_vars, goal_state) = self.get_lists()
-        self.planner = ActionPlanner(KnowledgeMap(state_vars, action_list), goal_state)
+        
+        km = KnowledgeMap()
+        if query['relation']['term'] in ('clinical outcome pathway', 'outcome pathway','clinical outcome path', 'outcome path'):
+            km.load_module('outcome_path')
+        elif query['relation']['term'] in ('protect', 'protects'):
+            km.load_module('protects_from')
+        
+        self.planner = ActionPlanner(km, km.default_goal)
         self.planner.make_plan(self.discount)
         
         if query['from']['bound'] == True:
@@ -39,6 +57,17 @@ class Agent:
             self.blackboard.add_node(query['to']['term'], entity = query['to']['entity'], name = query['to']['term'])
 
     def show_blackboard(self, width=2, height=2):
+        """Plot a figure showing the current blackboard content.
+        
+        Parameters
+        ----------
+        width : numeric, optional
+            width of plot (default: 2)
+        
+        height : numeric, optional
+            height of plot (default: 2)
+        
+        """
         plt.figure(figsize=(width, height))
         networkx.draw(self.blackboard, with_labels=True)
     
@@ -49,6 +78,15 @@ class Agent:
         return({'state':bound_nodes|connections, 'entities':entities})
     
     def observe_state(self):
+        """Return the current agent state.
+        
+        Returns
+        -------
+        dict
+            A dictionary with entries 'state' and 'entities', which are lists of the
+            current state variables and the unique entities they contain, respectively.
+        
+        """
         return(self.get_state(self.blackboard))
   
     def observe_path_state(self, start, end):
@@ -64,6 +102,15 @@ class Agent:
             return(path_states)
     
     def acquire_knowledge(self):
+        """Execute the current plan to query knowledge sources.
+        
+        Returns
+        -------
+        bool
+            True if knowledge acquisition was successful (i.e., the agent reached
+            a goal state), False if not.
+        
+        """
         # guard agains external changes to blackboard
         if len(self.planner.actions_used) > 0:
             print("Blackboard may have changed since last knowledge acquisition ... replanning")
@@ -112,15 +159,23 @@ class Agent:
         # use path graph to iterate but apply updates to blackboard
         networkx.set_edge_attributes(self.blackboard, stats)
 
-    def calculate_edge_probabilities(self, default_probability = 0):
+    def calculate_edge_probabilities(self):
         pgm = ConnectionPGM()
         variables = ['is_connection']
-
+        
+        evidence_types = ('pubmed',)
+        
+        # calculate default probabilities
+        default_probabilities = dict()
+        for evidence_type in evidence_types:
+            samples = pgm.evaluate(evidence_type, {}, variables)
+            default_probabilities[evidence_type] = pgm.get_mean(samples, 'is_connection')
+        
         current_year = int(datetime.datetime.now().year)
         attributes = dict()
         for (u, v, d) in self.blackboard.edges(data=True):
             if 'article_count' not in d or d['article_count'] == 0:
-                attributes[(u,v)] = {'p':default_probability,'1-p':1-default_probability}
+                sample_mean = default_probabilities['pubmed']
             else:
                 evidence = {'num_articles':d['article_count']}
                 if 'year_first_article' in d:
@@ -128,97 +183,55 @@ class Agent:
                     evidence['years_since_first_article'] = y
                 samples = pgm.evaluate('pubmed', evidence, variables)
                 sample_mean = pgm.get_mean(samples, 'is_connection')
-                attributes[(u,v)] = {'p':sample_mean,'1-p':1-sample_mean}
-                #print(u + ' ' + v + ' ' + str(pgm.get_mean(samples, 'is_connection')))
+                
+            attributes[(u,v)] = {'p':sample_mean,'1-p':1-sample_mean}
         networkx.set_edge_attributes(self.blackboard, attributes)
 
     def in_goal_state(self):
+        """Returns True if the agent is in a goal state.
+        
+        Returns
+        -------
+        boolean
+            Indicates whether the agent is in a goal state or not.
+        
+        """
         current_state = self.observe_state()['state']
         diff = set(self.planner.goal_state) - set(current_state)
         return len(diff) == 0
     
     def analyze(self, source, target):
+        """Analyze the knowledge on the blackboard and return the best path between source and target.
+        
+        Parameters
+        ----------
+        source : str
+            Name of the source node.
+            
+        target : str
+            Name of the target node.
+        
+        Returns
+        -------
+        path : list
+            An ordered list of nodes from source to target. Each node is represented by a dictionary of
+            node attributes, minimally including its 'name' and 'entity'. If no path was found, an
+            empty list is returned.
+        
+        """
         # create a subgraph that consists of all nodes on a path between source and target
         path_graph = self.blackboard.get_path_subgraph([source], [target])
         self.set_edge_stats(path_graph)
         self.calculate_edge_probabilities()
         try:
-            path = networkx.shortest_path(self.blackboard, source, target, '1-p')
-            path_data = list()
-            for node in path:
+            path_nodes = networkx.shortest_path(self.blackboard, source, target, '1-p')
+            path = list()
+            for node in path_nodes:
                 attributes = self.blackboard.nodes[node].copy()
                 if 'unbound' in attributes and attributes['unbound'] == True:
                     attributes['name'] = re.sub(r'_[0-9]+?$', '', attributes['name'])
-                #if attributes['entity'] in path_data:
-                #    raise Exception('Path already contains entity ' + attributes['entity'])
-                path_data.append(attributes)
-            return path_data
+                path.append(attributes)
+            return path
         except networkx.NetworkXNoPath:
-            print("no path exists.")
+            print("No path exists.")
             return []
-
-    def get_lists(self):
-        action_list =[
-            {
-                'action':DrugBankDrugToUniProtTarget(),
-                'p_success':0.5,
-                'reward':2
-            },
-            
-            {
-                'action':PharosDrugToTarget(),
-                'p_success':0.5,
-                'reward':1
-            },
-            
-            {
-                'action':PharosTargetToDisease(),
-                'p_success':0.5,
-                'reward':2
-            }, 
-
-            {
-                'action':GoFunctionTargetToPathway(),
-                'p_success':0.7,
-                'reward':1.2
-            },
-
-            {
-                'action':WikiPWTargetToPathway(),
-                'p_success':0.6,
-                'reward':2
-            },
-
-            {
-                'action':PubmedPathwayDiseasePath(),
-                'p_success':0.9,
-                'reward':2
-            },
-
-               {
-                'action':PubmedCellDiseasePath(),
-                'p_success':0.9,
-                'reward':1
-            },
-
-            {
-                'action':PubmedDiseaseToSymptom(),
-                'p_success':0.9,
-                'reward':0.5
-            },
-
-            {
-                'action':PubmedDrugDiseasePath(),
-                'p_success':0.99,
-                'reward':0.5
-            }
-        ]
-
-        state_vars =  ['bound(Drug)', 'bound(Target)', 'bound(Pathway)', 'bound(Cell)', 'bound(Symptom)', 'bound(Disease)',
-                      'connected(Drug, Target)', 'connected(Target, Pathway)', 'connected(Pathway, Cell)', 'connected(Cell, Symptom)', 'connected(Symptom, Disease)']
-
-
-        goal_state = ['bound(Drug)', 'bound(Target)', 'bound(Pathway)', 'bound(Cell)', 'bound(Symptom)', 'bound(Disease)',
-                                 'connected(Drug, Target)', 'connected(Target, Pathway)', 'connected(Pathway, Cell)', 'connected(Cell, Symptom)', 'connected(Symptom, Disease)']
-
-        return((action_list, state_vars, goal_state))
--- reasoner/KnowledgeMap.py
+++ reasoner/KnowledgeMap.py
@@ -1,4 +1,146 @@
+from .actions.edit_actions import *
+from .actions.eutils import *
+from .actions.sparql import *
+from .actions.pharos import *
+from .actions.file_actions import *
+
+
 class KnowledgeMap:
-    def __init__(self, state_variables, action_list):
-        self.state_variables = state_variables
-        self.actions = action_list
+    """
+    Store query actions and other information of how to interact with knowledge sources.
+    
+    The ``KnowledgeMap`` is a central repository for ``Action`` objects and their quality
+    metrics. The quality metrics are the (1) transition probabilities and (2) rewards used
+    by the ``ActionPlanner`` to calculate a plan (using a Markov decision process).
+
+    """
+    def __init__(self):
+        pass
+        
+    def load_module(self, module):
+        """Load a module into a ``KnowledgeMap`` object.
+        
+        Parameters
+        ----------
+        module : str
+            The name of the module to be loaded.
+        
+        """
+        modules = {
+            'protects_from':{
+                'actions':
+                    [
+                        {
+                            'action':EditGeneticConditionToDisease(),
+                            'p_success':1,
+                            'reward':0.01
+                        },
+
+                        {
+                            'action':ClinvarDiseaseToCondition(),
+                            'p_success':0.5,
+                            'reward':2
+                        },
+
+                        {
+                            'action':MeshConditionToGeneticCondition(),
+                            'p_success':0.5,
+                            'reward':2
+                        },
+
+                        {
+                            'action':MedGenConditionToGeneticCondition(),
+                            'p_success':0.7,
+                            'reward':1
+                        }
+                    ],
+            
+                'state_vars':
+                    ['bound(Disease)', 'bound(Variant)', 'bound(Gene)', 'bound(Condition)', 'bound(GeneticCondition)',
+                     'connected(Variant, GeneticCondition)', 'connected(Condition, GeneticCondition)',
+                     'connected(Variant, Gene)', 'connected(Disease, Variant)', 'connected(Variant, Condition)'],
+            
+                'goal_state':['bound(GeneticCondition)', 'bound(Disease)', 'bound(Variant)',
+                              'connected(Disease, Variant)', 'connected(Variant, GeneticCondition)']
+            },
+            
+            
+            'outcome_path':{
+                'actions': 
+                    [
+                        {
+                            'action':DrugBankDrugToTarget(),
+                            'p_success':0.5,
+                            'reward':3
+                        },
+                        
+                        {
+                            'action':PharosDrugToTarget(),
+                            'p_success':0.5,
+                            'reward':0.01 #2
+                        },
+                        
+                        {
+                            'action':DrugBankDrugToUniProtTarget(),
+                            'p_success':0.5,
+                            'reward':1
+                        },
+
+                        {
+                            'action':PharosTargetToDisease(),
+                            'p_success':0.5,
+                            'reward':0.01 #2
+                        }, 
+
+                        {
+                            'action':GoFunctionTargetToPathway(),
+                            'p_success':0.7,
+                            'reward':1.2
+                        },
+
+                        {
+                            'action':WikiPWTargetToPathway(),
+                            'p_success':0.6,
+                            'reward':2
+                        },
+
+                        {
+                            'action':PubmedPathwayDiseasePath(),
+                            'p_success':0.9,
+                            'reward':2
+                        },
+
+                           {
+                            'action':PubmedCellDiseasePath(),
+                            'p_success':0.9,
+                            'reward':1
+                        },
+
+                        {
+                            'action':PubmedDiseaseToSymptom(),
+                            'p_success':0.9,
+                            'reward':0.5
+                        },
+
+                        {
+                            'action':PubmedDrugDiseasePath(),
+                            'p_success':0.99,
+                            'reward':0.5
+                        }
+                    ],
+                
+                'state_vars':
+                    ['bound(Drug)', 'bound(Target)', 'bound(Pathway)', 'bound(Cell)', 'bound(Symptom)',
+                     'bound(Disease)', 'connected(Drug, Target)', 'connected(Target, Pathway)',
+                     'connected(Pathway, Cell)', 'connected(Cell, Symptom)', 'connected(Symptom, Disease)'],
+                
+                'goal_state':
+                    ['bound(Drug)', 'bound(Target)', 'bound(Pathway)', 'bound(Cell)', 'bound(Symptom)',
+                     'bound(Disease)', 'connected(Drug, Target)', 'connected(Target, Pathway)',
+                     'connected(Pathway, Cell)', 'connected(Cell, Symptom)', 'connected(Symptom, Disease)']
+            }
+        }
+        
+        self.state_variables = modules[module]['state_vars']
+        self.actions = modules[module]['actions']
+        self.default_goal = modules[module]['goal_state']
