f4fb7842e5fb5bc77a6652a4fb2d61edb4e1c884
vdancik <vdancik@broadinstitute.org>
2017-11-28 15:57:53 -0500

    revert to originan ClinVar actions, add gene-to-condition action

--- reasoner/actions/clinvar.py
+++ reasoner/actions/clinvar.py
@@ -195,64 +195,8 @@ class ClinVarDiseaseToConditionOld(XmlApiAction):
         return variants
 
 
-class EutilitiesAction(Action):
-    def __init__(self, precondition, effect):
-        super().__init__(precondition, effect)
-        self.url_prefix = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
 
-    def get_request(self, url):
-        with urllib.request.urlopen(url) as response:
-            res = response.read().decode()
-        return res
-
-    def get_json_request(self, url):
-        return self.parse_json(self.get_request(url))
-
-    def get_xml_request(self, url):
-        return self.parse_xml(self.get_request(url))
-
-    def parse_json(self, obj):
-        return json.loads(obj)
-
-    def parse_xml(self, obj):
-        return etree.fromstring(obj)
-
-    def esearch(self, term, db, retstart = 0, retmax = 20, retmode = "json", quote = True, sort = None, count = False):
-        if quote == True:
-            term = urllib.parse.quote_plus(term)
-        query = 'esearch.fcgi?db=' + db +'&term=' + term + '&retmode=' + retmode + '&retstart=' + str(retstart) + '&retmax=' + str(retmax)
-
-        if sort is not None:
-            query = query + '&sort=' + sort
-        if count == True:
-            query = query + '&rettype=count'
-
-        res = self.get_json_request(self.url_prefix + query)
-        return res
-
-    def esummary(self, entry_id, db, retmode="json"):
-        query = 'esummary.fcgi?db=' + db +'&id=' + entry_id + '&retmode=' + retmode
-        res = self.get_json_request(self.url_prefix + query)
-        return res
-
-    def efetch(self, entry_id, db, rettype = None, retmode = None):
-        query = 'efetch.fcgi?db=' + db + '&id=' + entry_id
-        if rettype is not None:
-            query = query + '&rettype=' + rettype
-        if retmode is not None:
-            query = query + '&retmode=' + retmode
-
-        url = self.url_prefix + query
-        print(url)
-        with urllib.request.urlopen(url) as response:
-            res = response.read()
-        return res
-
-    def add_quotation_marks(self, query):
-        return '"%s"' % query
-
-
-class ClinvarAction(EutilitiesAction):
+class ClinvarActionBad(Action):
     def __init__(self, precondition, effect, additional_term=''):
         super().__init__(precondition, effect)
         self.additional_term = additional_term
@@ -324,7 +268,7 @@ class ClinvarAction(EutilitiesAction):
             variant['Condition'] = [x for i,x in enumerate(variant['Condition']) if i not in remove_idx]
         return(variants)
 
-class ClinvarDiseaseToCondition(ClinvarAction):
+class ClinvarDiseaseToConditionBad(ClinvarActionBad):
     """Find a all conditions connected to a disease via a common variant in ClinVar.
     """
     def __init__(self, additional_search_term = '+protective'):
@@ -370,7 +314,7 @@ class ClinvarDiseaseToCondition(ClinvarAction):
 
 
 
-class ClinvarDiseaseToGene(ClinvarAction):
+class ClinvarDiseaseToGeneBad(ClinvarActionBad):
     """Find a all conditions connected to a disease via a common variant in ClinVar.
     """
     def __init__(self, additional_search_term = '+protective'):
@@ -408,7 +352,7 @@ class ClinvarDiseaseToGene(ClinvarAction):
 
 
 
-class ClinvarGeneToCondition(ClinvarAction):
+class ClinvarGeneToConditionBad(ClinvarActionBad):
     """Find a all conditions connected to a gene via a common variant in ClinVar.
     """
     def __init__(self, additional_search_term = ' and (("clinsig pathogenic"[Properties]) OR ("clinsig likely pathogenic"[Properties]) OR ("clinsig risk factor"[Properties]))'):
@@ -444,5 +388,235 @@ class ClinvarGeneToCondition(ClinvarAction):
             variant_out['Condition'] = conditions
 
         return variant_out
+
+class EutilitiesAction(Action):
+    def __init__(self, precondition, effect):
+        super().__init__(precondition, effect)
+        self.url_prefix = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
+
+    def get_request(self, url):
+        with urllib.request.urlopen(url) as response:
+            res = response.read().decode()
+        return res
+
+    def get_json_request(self, url):
+        return self.parse_json(self.get_request(url))
+
+    def get_xml_request(self, url):
+        return self.parse_xml(self.get_request(url))
+
+    def parse_json(self, obj):
+        return json.loads(obj)
+
+    def parse_xml(self, obj):
+        return etree.fromstring(obj)
+
+    def esearch(self, term, db, retstart = 0, retmax = 20, retmode = "json", quote = True, sort = None, count = False):
+        if quote == True:
+            term = urllib.parse.quote_plus(term)
+        query = 'esearch.fcgi?db=' + db +'&term=' + term + '&retmode=' + retmode + '&retstart=' + str(retstart) + '&retmax=' + str(retmax)
+
+        if sort is not None:
+            query = query + '&sort=' + sort
+        if count == True:
+            query = query + '&rettype=count'
+
+        res = self.get_json_request(self.url_prefix + query)
+        return res
+
+    def esummary(self, entry_id, db, retmode="json"):
+        query = 'esummary.fcgi?db=' + db +'&id=' + entry_id + '&retmode=' + retmode
+        res = self.get_json_request(self.url_prefix + query)
+        return res
+
+    def efetch(self, entry_id, db, rettype = None, retmode = None):
+        query = 'efetch.fcgi?db=' + db + '&id=' + entry_id
+        if rettype is not None:
+            query = query + '&rettype=' + rettype
+        if retmode is not None:
+            query = query + '&retmode=' + retmode
+
+        url = self.url_prefix + query
+        print(url)
+        with urllib.request.urlopen(url) as response:
+            res = response.read()
+        return res
+
+    def add_quotation_marks(self, query):
+        return '"%s"' % query
+
+
+class ClinvarAction(EutilitiesAction):
+    def __init__(self, precondition, effect, additional_term=''):
+        super().__init__(precondition, effect)
+        self.additional_term = additional_term
+
+    def find_disease(self, variants, query_term):
+        for variant in variants:
+            if 'Condition' not in variant.keys():
+                continue
+            remove_idx = list()
+            for i in range(len(variant['Condition'])):
+                if query_term in variant['Condition'][i]['node']['name'].lower():
+                    variant['Variant'][0]['edge'] = variant['Condition'][i]['edge']
+                    remove_idx.append(i)
+            variant['Condition'] = [x for i,x in enumerate(variant['Condition']) if i not in remove_idx]
+        return(variants)
+
+
+    def parse_conditions(self, germline_elem):
+        conditions = []
+        for phenotype_elem in germline_elem.findall('./PhenotypeList/Phenotype'):
+            condition = {'node':{'name':phenotype_elem.attrib['Name']},'edge':{}}
+            for xref_elem in phenotype_elem.findall('./XRefList/XRef'):
+                if 'id' not in condition['node'] or xref_elem.attrib['DB']=='MedGen':
+                    condition['node']['id'] = xref_elem.attrib['ID']
+                    condition['node']['authority'] = xref_elem.attrib['DB']
+            if germline_elem.find('ReviewStatus') != None:
+                condition['edge']['review_status'] = germline_elem.find('ReviewStatus').text
+            if germline_elem.find('./ClinicalSignificance/Description')  != None:
+                condition['edge']['clinical_significance'] = germline_elem.find('./ClinicalSignificance/Description').text
+                if germline_elem.find('./ClinicalSignificance/Citation/ID') != None:
+                    condition['edge']['pmid'] = germline_elem.find('./ClinicalSignificance/Citation/ID').text
+            if condition['node']['name'] != 'not specified' and condition['node']['name'] != 'not provided' :
+                conditions.append(condition)
+        return conditions
+
+
+    def parse_gene(self, gene_elem):
+        gene = {}
+        gene['node'] = {'id':gene_elem.attrib['GeneID'],'name':gene_elem.attrib['Symbol'], 'authority':'Entrez:GeneID'}
+        gene['edge'] = {'relationship': gene_elem.attrib['RelationshipType']}
+        return gene
+
+
+    def load_variants(self, root):
+        variants = []
+        for child in root.findall('./VariationReport'):
+            variant = self.parse_variant(child)
+            if variant != None:
+                variants.append(variant)
+        return variants
+
+
+class ClinvarDiseaseToCondition(ClinvarAction):
+    """Find a all conditions connected to a disease via a common variant in ClinVar.
+    """
+    def __init__(self, additional_search_term = '+protective'):
+        super().__init__(['bound(Disease)'],
+        ['bound(Variant) and connected(Disease, Variant)',
+        'bound(Variant) and connected(Disease, Variant) and bound(Gene) and connected(Variant, Gene)',
+        'bound(Variant) and connected(Disease, Variant) and bound(Condition) and connected(Variant, Condition)'
+        ],
+        additional_search_term)
+
+    def execute(self, query):
+        search_results = self.esearch(self.add_quotation_marks(query['Disease'])+'[dis]'+self.additional_term, 'clinvar', retmax = 500)
+        if len(search_results['esearchresult']['idlist']) == 0:
+            return {}
+
+        uid = ','.join(search_results['esearchresult']['idlist'])
+        fetch_result = self.parse_xml(self.efetch(uid, 'clinvar', rettype='variation'))
+        variants = self.load_variants(fetch_result)
+        result = self.find_disease(variants, query['Disease'])
+        return result
+
+
+    def parse_variant(self, variant_elem):
+
+        variant_name = variant_elem.attrib['VariationName']
+        variant_id = variant_elem.attrib['VariationID']
+        variant = {'Variant':[{'node':{'id':variant_id,'name':variant_name, 'authority':'ClinVar:VariationID'},'edge':{}}]}
+
+        genes = [self.parse_gene(gene_elem) for gene_elem in variant_elem.findall('./GeneList/Gene')]
+        if len(genes) > 0:
+            variant['Gene'] = genes
+            if len(genes) == 1:
+                variant['Variant'][0]['node']['symbol'] = genes[0]['node']['name']
+
+        conditions = []
+        for germline_elem in variant_elem.findall('./ClinicalAssertionList/GermlineList/Germline'):
+            conditions.extend(self.parse_conditions(germline_elem))
+        if len(conditions) > 0:
+            variant['Condition'] = conditions
+        return variant
+
+
+
+class ClinvarDiseaseToGene(ClinvarAction):
+    """Find a all conditions connected to a disease via a common variant in ClinVar.
+    """
+    def __init__(self, additional_search_term = '+protective'):
+        super().__init__(['bound(Disease)'],
+        ['bound(Variant) and connected(Disease, Variant)',
+        'bound(Variant) and connected(Disease, Variant) and bound(Gene) and connected(Variant, Gene)'
+        ],
+        additional_search_term)
+
+    def execute(self, query):
+        search_results = self.esearch(self.add_quotation_marks(query['Disease'])+'[dis]'+self.additional_term, 'clinvar', retmax = 500)
+        if len(search_results['esearchresult']['idlist']) == 0:
+            return {}
+
+        uid = ','.join(search_results['esearchresult']['idlist'])
+        fetch_result = self.parse_xml(self.efetch(uid, 'clinvar', rettype='variation'))
+        variants = self.load_variants(fetch_result)
+        return variants
+
+
+    def parse_variant(self, variant_elem):
+
+        variant_name = variant_elem.attrib['VariationName']
+        variant_id = variant_elem.attrib['VariationID']
+        variant = {'Variant':[{'node':{'id':variant_id,'name':variant_name, 'authority':'ClinVar:VariationID'},'edge':{}}]}
+
+        genes = [self.parse_gene(gene_elem) for gene_elem in variant_elem.findall('./GeneList/Gene')]
+        if len(genes) > 0:
+            variant['Gene'] = genes
+            if len(genes) == 1:
+                variant['Variant'][0]['node']['symbol'] = genes[0]['node']['name']
+
+        return variant
+
+
+class ClinvarGeneToCondition(ClinvarAction):
+    """Find a all conditions connected to a gene via a common variant in ClinVar.
+    """
+    def __init__(self, additional_search_term = ' and (("clinsig pathogenic"[Properties]) OR ("clinsig likely pathogenic"[Properties]) OR ("clinsig risk factor"[Properties]))'):
+        super().__init__(['bound(Gene)'],
+        ['bound(Variant) and connected(Gene, Variant) and bound(Condition) and connected(Variant, Condition)'
+        ],
+        additional_search_term)
+
+
+    def execute(self, query):
+        search_results = self.esearch(self.add_quotation_marks(query['Gene'])+'[gene]'+self.additional_term, 'clinvar', retmax = 500)
+        if len(search_results['esearchresult']['idlist']) == 0:
+            return {}
+
+        uid = ','.join(search_results['esearchresult']['idlist'])
+        fetch_result = self.parse_xml(self.efetch(uid, 'clinvar', rettype='variation'))
+        variants = self.load_variants(fetch_result)
+        return variants
+
+
+    def parse_variant(self, variant_elem):
+
+        variant_name = variant_elem.attrib['VariationName']
+        variant_id = variant_elem.attrib['VariationID']
+        variant = {'Variant':[{'node':{'id':variant_id,'name':variant_name, 'authority':'ClinVar:VariationID'},'edge':{}}]}
+
+        conditions = []
+        for germline_elem in variant_elem.findall('./ClinicalAssertionList/GermlineList/Germline'):
+            conditions.extend(self.parse_conditions(germline_elem))
+        if len(conditions) == 0:
+            return None
+        variant['Condition'] = conditions
+
+        genes = [self.parse_gene(gene_elem) for gene_elem in variant_elem.findall('./GeneList/Gene')]
+        if len(genes) == 1:
+            variant['Variant'][0]['node']['symbol'] = genes[0]['node']['name']
+
+        return variant
 
 
