66dde8afae2c9a8259f03fb4df18352cc6469173
Mathias Wawer <mwawer@broadinstitute.org>
2018-03-28 16:12:57 -0400

    add GO pathways (MSigDB C5 BP and MF)

--- neotest/get_pathway_cui.py
+++ neotest/get_pathway_cui.py
@@ -11,13 +11,29 @@ def add_pathway(tx, cui, name, original_name):
            cui=cui, name=name, original_name=original_name)
 
 
+def query_umls(uq, query_term):
+    result = uq.search(query_term)
+    if result:
+        cui = result['results'][0]['ui']
+        name = result['results'][0]['name']
+
+        if cui == 'NONE':
+            return {}
+
+        return({'cui': cui,
+                'name': name,
+                'original_name': ori_name})
+
+
 config = Config().config
 apikey = Config().config['umls']['apikey']
 
-path_file = './data/c2.cp.v6.1.entrez.gmt'
+path_file = './data/c2cp_c5mf_c5bp_v6.1.entrez.gmt'
 outfile = './data/cop_pathway_cui.csv'
 
-driver = GraphDatabase.driver(config['neo4j']['host'], auth=(config['neo4j']['user'], config['neo4j']['password']))
+driver = GraphDatabase.driver(
+    config['neo4j']['host'],
+    auth=(config['neo4j']['user'], config['neo4j']['password']))
 
 uq = UmlsQuery(apikey)
 cuis = pd.DataFrame(columns=["cui", "name", "original_name"])
@@ -26,18 +42,23 @@ with driver.session() as session:
         for line in f:
             parts = line.strip().split('\t')
             ori_name = parts[0]
-            query_term = ' '.join(ori_name.split('_')[1:]).lower() + ' pathway'
 
-            result = uq.search(query_term)
+            query_base = ' '.join(ori_name.split('_')[1:]).lower()
+            result = query_umls(uq, query_base + ' pathway')
             if result:
-                cui = result['results'][0]['ui']
-                name = result['results'][0]['name']
-                print(name)
-                if cui != 'NONE':
-                    cuis = cuis.append({'cui': cui,
-                                        'name': name,
-                                        'original_name': ori_name},
-                                       ignore_index=True)
-                    session.write_transaction(add_pathway, cui, name, ori_name)
+                cuis = cuis.append(result)
+                session.write_transaction(
+                    add_pathway,
+                    result['cui'],
+                    result['name'], result['ori_name'])
+            else:
+                query_term = ' '.join(ori_name.split('_')[1:]).lower()
+                result = query_umls(uq, query_base)
+                if result:
+                    cuis = cuis.append(result)
+                    session.write_transaction(
+                        add_pathway, result['cui'],
+                        result['name'],
+                        result['ori_name'])
 
 cuis.to_csv(outfile, index=False)
