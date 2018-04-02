4cf4323821dee1284e7a2740ad0f44cded9bdae6
Mathias Wawer <mwawer@broadinstitute.org>
2018-04-02 14:41:28 -0400

    create get_disease_finding_sites.py

---
+++ neotest/get_disease_finding_sites.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import mysql.connector
+from neo4j.v1 import GraphDatabase
+from Config import Config
+
+
+def db_select(db, sql):
+    cursor = db.cursor(dictionary=True)
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+    except:
+        print("Error: unable to fetch data")
+
+    return(results)
+
+
+# Open database connection
+config = Config().config
+db = mysql.connector.connect(user=config['semmeddb']['user'], password=config['semmeddb']['password'],
+                              host=config['semmeddb']['host'],
+                              database=config['semmeddb']['database'])
+
+sql = ("select distinct cui1 as location_cui, RELA as relation, "
+       "cui2 as disease_cui, A.str as location_str, B.str as disease_str, MRREL.sab as source "
+       "from MRREL "
+       "inner join MRSTY "
+       "on MRREL.cui2 = MRSTY.cui "
+       "left join MRCONSO A on A.cui = MRREL.cui1 "
+       "left join MRCONSO B on B.cui = MRREL.cui2 "
+       "where tui = 'T047' "
+       "and RELA = 'has_finding_site' "
+       "and A.ispref = 'Y' "
+       "and A.TS = 'P' "
+       "and A.lat='ENG' "
+       "and A.stt = 'PF' "
+       "and B.ispref = 'Y' "
+       "and B.TS = 'P' "
+       "and B.lat='ENG' "
+       "and B.stt = 'PF' "
+       "limit 10;")
+
+result = db_select(db, sql)
+print(result)
