326083c8785d2966e2215baea431054803304b95
Mathias Wawer <mwawer@broadinstitute.org>
2018-04-15 23:11:15 -0400

    change load_pathways to write to csv, then use cypher

---
+++ neotest/load_pathways.cypher
@@ -0,0 +1,8 @@
+LOAD CSV WITH HEADERS FROM 'file:///pathways.csv' AS line
+MERGE (pathway:Pathway {id: line.cui})
+SET pathway.name = line.umls_name;
+
+LOAD CSV WITH HEADERS FROM 'file:///pathway_targets.csv' AS line
+MATCH (pathway:Pathway {id: line.pathway_cui})
+MATCH (target:Target {entrez_id: line.target_entrez_id})
+MERGE (target)-[:PART_OF]->(pathway)
--- neotest/load_pathways.py
+++ neotest/load_pathways.py
@@ -1,22 +1,6 @@
-import pandas as pd
+import csv
 from umls.UmlsQuery import UmlsQuery
-from neo4j.v1 import GraphDatabase
 from Config import Config
-
-def add_pathway(tx, cui, umls_name, target_entrez_ids):
-    tx.run("MERGE (pathway:Pathway {id: {cui}}) "
-           "SET pathway.name = {umls_name} "
-           "WITH pathway "
-           "UNWIND {target_entrez_ids} as target_entrez_id "
-           "MATCH (target:Target {entrez_id: target_entrez_id}) "
-           "MERGE (target)-[:PART_OF]->(pathway)",
-        cui=cui, umls_name=umls_name, target_entrez_ids=target_entrez_ids)
-
-
-# def get_pathway_names(session):
-#     result = session.run("MATCH (p:Pathway) RETURN p.msigdb_name as msigdb_name")
-#     return([record['msigdb_name'] for record in result])
-
 
 def query_umls(uq, query_term):
     result = uq.search(query_term)
@@ -32,12 +16,11 @@ def query_umls(uq, query_term):
 
 config = Config().config
 apikey = config['umls']['apikey']
-driver = GraphDatabase.driver(
-    config['neo4j']['host'],
-    auth=(config['neo4j']['user'], config['neo4j']['password']))
 
 msigdb_file = './data/c2cp_c5mf_c5bp_v6.1.entrez.gmt'
-outfile = './data/cop_pathway_cui.csv'
+outfile_pathways = './data/graph/pathways.csv'
+outfile_pathway_targets = './data/graph/pathway_targets.csv'
+outfile_not_found = './data/pathways_not_found.txt'
 
 pathways = []
 with open(msigdb_file) as f:
@@ -46,24 +29,35 @@ with open(msigdb_file) as f:
         pathways.append({'name':parts[0], 'url':parts[1], 'entrez_ids':parts[2:]})
 
 uq = UmlsQuery(apikey)
-cuis = pd.DataFrame(columns=["cui", "umls_name", "msigdb_name"])
-with driver.session() as session:
-    #existing_pathways = get_pathway_names(session)
-    for pathway in pathways:
-        
-        # if pathway['name'] in existing_pathways:
-        #     continue
+cuis = [["cui", "umls_name", "msigdb_name"]]
+not_found = []
+pathway_targets = [["pathway_cui", "target_entrez_id"]]
 
-        query_base = ' '.join(pathway['name'].split('_')[1:]).lower()
-        result = query_umls(uq, query_base + ' pathway')
-        if not result:
-            result = query_umls(uq, query_base)
+for pathway in pathways:
 
-        if result:
-            cuis = cuis.append({'cui':result['cui'], 'umls_name':result['name'], 'msigdb_name':pathway['name']}, ignore_index=True)
-            session.write_transaction(add_pathway, result['cui'], result['name'], pathway['entrez_ids'])
-        else:
-            cuis = cuis.append({'cui':'na', 'umls_name':'na', 'msigdb_name':pathway['name']}, ignore_index=True)
+    query_base = ' '.join(pathway['name'].split('_')[1:]).lower()
+    result = query_umls(uq, query_base + ' pathway')
+    if not result:
+        result = query_umls(uq, query_base)
 
-cuis.to_csv(outfile, index=False)
+    if result:
+        cuis.append([result['cui'], result['name'], pathway['name']])
+        for target in pathway['entrez_ids']:
+            pathway_targets.append([result['cui'], target])
+    else:
+        not_found.append(pathway['name'])
+
+
+with open(outfile_pathways, 'w') as f:  
+   writer = csv.writer(f)
+   writer.writerows(cuis)
+
+with open(outfile_pathway_targets, 'w') as f:  
+   writer = csv.writer(f)
+   writer.writerows(pathway_targets)
+
+with open(outfile_not_found, 'w') as f:
+    for item in not_found:
+        f.write("%s\n" % item)
+
 print('done')
