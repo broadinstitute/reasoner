61fba2a22a1b119239e24545d8f8ed76355de1e6
Mathias Wawer <mwawer@broadinstitute.org>
2018-04-24 12:22:52 -0400

    create setup.py

---
+++ reasoner/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='reasoner',
+      version='0.0.1',
+      description='Broad Translator Reasoner',
+      url='http://github.com/broadinstitute/reasoner',
+      author='Mathias Wawer',
+      author_email='mwawer@broadinstitute.org',
+      license='MIT',
+      packages=['reasoner'],
+      zip_safe=False)
