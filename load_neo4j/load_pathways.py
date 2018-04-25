79532e050bf897db68725272420a0b59fdee11b5
Mathias Wawer <mwawer@broadinstitute.org>
2018-04-25 15:47:58 -0400

    rename load_go.py to load_pathways.py

--- load_neo4j/load_pathways.py
+++ load_neo4j/load_pathways.py
@@ -1,60 +1,33 @@
-import csv
+import pandas
 from reasoner.neo4j.umls.UmlsQuery import UmlsQuery
 
-def query_umls(uq, query_term):
-    result = uq.search(query_term)
-    if result:
-        cui = result['results'][0]['ui']
-        name = result['results'][0]['name']
+go_file = "../data/neo4j/goa_human.csv"
+outfile = "../data/neo4j/graph/pathways.csv"
 
-        if cui == 'NONE':
-            return {}
+go = pandas.read_csv(go_file, low_memory = False)
 
-        return({'cui': cui, 'name': name})
+go = go[['db', 'db_object_id', 'go_id', 'evidence_code', 'aspect']]
+go = go[(go['db'] == 'UniProtKB') & ((go['aspect'] == 'P') | (go['aspect'] == 'F'))]
 
-
-
-msigdb_file = './data/c2cp_c5mf_c5bp_v6.1.entrez.gmt'
-outfile_pathways = './data/graph/pathways.csv'
-outfile_pathway_targets = './data/graph/pathway_targets.csv'
-outfile_not_found = './data/pathways_not_found.txt'
-
-pathways = []
-with open(msigdb_file) as f:
-    for line in f:
-        parts = line.strip().split('\t')
-        pathways.append({'name':parts[0], 'url':parts[1], 'entrez_ids':parts[2:]})
 
 uq = UmlsQuery()
-cuis = [["cui", "umls_name", "msigdb_name"]]
-not_found = []
-pathway_targets = [["pathway_cui", "target_entrez_id"]]
 
-for pathway in pathways:
+unique_goids = go['go_id'].unique()
+nids = len(unique_goids)
+cui_list = ['']*nids
+name_list = ['']*nids
+for i, go_id in enumerate(unique_goids):
+    print(go_id)
+    result = uq.go2cui(go_id)
+    if len(result) > 1:
+        print(result)
+        raise ValueError
+    if len(result) == 1:
+        cui_list[i] = result[0]['cui']
+        name_list[i] = result[0]['name']
 
-    query_base = ' '.join(pathway['name'].split('_')[1:]).lower()
-    result = query_umls(uq, query_base + ' pathway')
-    if not result:
-        result = query_umls(uq, query_base)
+go2cui = pandas.DataFrame({'go_id': unique_goids, 'cui': cui_list, 'name': name_list})
 
-    if result:
-        cuis.append([result['cui'], result['name'], pathway['name']])
-        for target in pathway['entrez_ids']:
-            pathway_targets.append([result['cui'], target])
-    else:
-        not_found.append(pathway['name'])
+go = go.join(go2cui.set_index('go_id'), on = 'go_id')
 
-
-with open(outfile_pathways, 'w') as f:  
-   writer = csv.writer(f)
-   writer.writerows(cuis)
-
-with open(outfile_pathway_targets, 'w') as f:  
-   writer = csv.writer(f)
-   writer.writerows(pathway_targets)
-
-with open(outfile_not_found, 'w') as f:
-    for item in not_found:
-        f.write("%s\n" % item)
-
-print('done')
+go.to_csv(outfile, index = False)
