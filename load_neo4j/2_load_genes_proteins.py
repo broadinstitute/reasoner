4c0c2c91e2e34a875acaf926d566080b7ccaf8dc
Mathias Wawer <mwawer@broadinstitute.org>
2018-09-14 19:12:55 -0400

    bugfix

--- load_neo4j/2_load_genes_proteins.py
+++ load_neo4j/2_load_genes_proteins.py
@@ -6,7 +6,7 @@ kg = KnowledgeGraph()
 with open(gene_file) as f:
     first_line = f.readline()
     for line in f:
-        items = line.strip().split('\t')
+        items = line.strip('\n').split('\t')
         kg.add_gene(hgnc_id=items[0], hgnc_symbol=items[1], entrez_id=items[3], name=items[2])
         kg.add_protein(uniprot_id=items[4], name=items[2])
         kg.add_gene_product_relation(hgnc_id=items[0], uniprot_id=items[4])
