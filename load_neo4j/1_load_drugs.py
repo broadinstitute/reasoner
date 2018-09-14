5dfae0e2bd171dbd2a06cd6a3241fec4a67e2c8b
Mathias Wawer <mwawer@broadinstitute.org>
2018-09-14 18:27:43 -0400

    bugfix

--- load_neo4j/1_load_drugs.py
+++ load_neo4j/1_load_drugs.py
@@ -6,8 +6,6 @@ drugs_file = '../data/knowledge_graph/ready_to_load/drugs.csv'
 drugs = pd.read_csv(drugs_file)
 kg = KnowledgeGraph()
 
+drugs.fillna(inplace=True)
 for index, row in drugs.iterrows():
-    for key, value in row.items():
-        if value == '':
-            row[key] = None
     kg.add_drug(row['cui'], row['chembl_id'], row['chebi_id'], row['drugbank_id'], row['name'], row['type'], row['mechanism'], row['pharmacodynamics'])
