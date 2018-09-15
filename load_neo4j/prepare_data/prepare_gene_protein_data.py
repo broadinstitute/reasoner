2ed75d3d76762d44bf14c140d896b9899d3a9a9a
Mathias Wawer <mwawer@broadinstitute.org>
2018-09-15 14:44:11 -0400

    fix type conversion

--- load_neo4j/prepare_data/prepare_gene_protein_data.py
+++ load_neo4j/prepare_data/prepare_gene_protein_data.py
@@ -3,9 +3,9 @@ import pandas as pd
 gene_file = '../data/knowledge_graph/primary/hgnc_genes_proteins.txt'
 outfile = '../data/knowledge_graph/ready_to_load/hgnc_genes_proteins.csv'
 
-drugs = pd.read_csv(gene_file, sep='\t')
+drugs = pd.read_csv(gene_file, sep='\t', , dtype=object)
 
-drugs['entrez_id'] = 'NCBIGENE:' + drugs['entrez_id'].astype(str)
-drugs['uniprot_id'] = 'UNIPROT:' + drugs['uniprot_id'].astype(str)
+drugs['entrez_id'] = 'NCBIGENE:' + drugs['entrez_id']
+drugs['uniprot_id'] = 'UNIPROT:' + drugs['uniprot_id']
 
 drugs.to_csv(outfile)
