7ef4cd36653e41fbc5f9f2a4d118bed4f95af3ac
Mathias Wawer <mwawer@broadinstitute.org>
2018-09-15 14:45:21 -0400

    remove index

--- load_neo4j/prepare_data/prepare_gene_protein_data.py
+++ load_neo4j/prepare_data/prepare_gene_protein_data.py
@@ -8,4 +8,4 @@ drugs = pd.read_csv(gene_file, sep='\t', dtype=object)
 drugs['entrez_id'] = 'NCBIGENE:' + drugs['entrez_id']
 drugs['uniprot_id'] = 'UNIPROT:' + drugs['uniprot_id']
 
-drugs.to_csv(outfile)
+drugs.to_csv(outfile, index=False)
