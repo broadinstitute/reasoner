2899d2861243a60039fe64461fffb658a46a25d6
Mathias Wawer <mwawer@broadinstitute.org>
2018-09-15 14:44:31 -0400

    bugfix

--- load_neo4j/prepare_data/prepare_gene_protein_data.py
+++ load_neo4j/prepare_data/prepare_gene_protein_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 gene_file = '../data/knowledge_graph/primary/hgnc_genes_proteins.txt'
 outfile = '../data/knowledge_graph/ready_to_load/hgnc_genes_proteins.csv'
 
-drugs = pd.read_csv(gene_file, sep='\t', , dtype=object)
+drugs = pd.read_csv(gene_file, sep='\t', dtype=object)
 
 drugs['entrez_id'] = 'NCBIGENE:' + drugs['entrez_id']
 drugs['uniprot_id'] = 'UNIPROT:' + drugs['uniprot_id']
