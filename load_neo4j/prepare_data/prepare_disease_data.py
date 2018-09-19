7e01d4062b544537f118d5766633785497182fb9
Mathias Wawer <mwawer@broadinstitute.org>
2018-09-19 09:18:14 -0700

    remove duplicates

--- load_neo4j/prepare_data/prepare_disease_data.py
+++ load_neo4j/prepare_data/prepare_disease_data.py
@@ -13,10 +13,11 @@ kg = KnowledgeGraph()
 uq = UmlsQuery()
 ct = ChemblTools()
 
+added = set()
 disease_data = pd.DataFrame(columns=["cui", "name", "hpo_id", "mesh_id"])
 for term in unique_diseases:
     result = uq.meshterm2cui(term)
-    if result:
+    if result and result[0]['mesh_id'] not in added:
         cui = result[0]['cui']
         mesh_id = result[0]['mesh_id']
         name = uq.cui2bestname(cui)[0]['name']
@@ -25,31 +26,35 @@ for term in unique_diseases:
             hpo_id = hpo_result[0]['hpo_id']
         else:
             hpo_id = ''
+        added.add(result[0]['mesh_id'])
         disease_data = disease_data.append({'cui': 'UMLS:' + cui,
                                             'name': name,
                                             'hpo_id': hpo_id,
                                             'mesh_id': 'MESH:' + mesh_id},
                                            ignore_index=True)
 
-
 chembl_ids = kg.get_drug_chembl_ids()
 for chembl_id in chembl_ids:
     indications = ct.get_indication(chembl_id.replace('CHEMBL:', ''))
     for row in indications:
         mesh_id = row['mesh_id']
-        result = uq.mesh2cui(mesh_id)
-        if result:
-            cui = result[0]['cui']
-            name = uq.cui2bestname(cui)[0]['name']
-            hpo_result = uq.cui2hpo(cui)
-            if hpo_result:
-                hpo_id = hpo_result[0]['hpo_id']
-            else:
-                hpo_id = ''
-            disease_data = disease_data.append({'cui': 'UMLS:' + cui,
-                                                'name': name,
-                                                'hpo_id': hpo_id,
-                                                'mesh_id': 'MESH:' + mesh_id},
-                                               ignore_index=True)
+        if mesh_id not in added:
+            result = uq.mesh2cui(mesh_id)
+            if result:
+                cui = result[0]['cui']
+                name = uq.cui2bestname(cui)[0]['name']
+                hpo_result = uq.cui2hpo(cui)
+                if hpo_result:
+                    hpo_id = hpo_result[0]['hpo_id']
+                else:
+                    hpo_id = ''
+                addded.add(mesh_id)
+                disease_data = disease_data.append({'cui': 'UMLS:' + cui,
+                                                    'name': name,
+                                                    'hpo_id': hpo_id,
+                                                    'mesh_id': 'MESH:' + mesh_id},
+                                                   ignore_index=True)
+
+#disease_data.drop_duplicates()
 
 disease_data.to_csv(outfile, index=False)
