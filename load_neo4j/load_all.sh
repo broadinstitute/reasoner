4804d7bdf6e7267ac75abf6fedcfa8178981e19b
Mathias Wawer <mwawer@broadinstitute.org>
2018-09-20 15:38:45 -0700

    add data preparation

--- load_neo4j/load_all.sh
+++ load_neo4j/load_all.sh
@@ -4,10 +4,17 @@ user=$1
 password=$2
 
 cypher-shell -u $user -p $password < create_indexes.cypher
+mysql semmeddb < prepare_data/prepare_semmeddb.sql
+
+python prepare_data/prepare_drugs.py
+python prepare_data/prepare_disease_data.py
+python prepare_data/prepare_gene_protein_data.py
+python prepare_data/prepare_go_target_to_pathway.py
 
 python load_drugs.py
 python load_genes_proteins.py
 python load_drug_target_relations.py
+python load_pathways.py
 python load_diseases.py
 python load_indications.py
 python load_semmeddb_data.py
