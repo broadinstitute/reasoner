832ba4b1013e5c6c60d680aee29c32ff10a63a6b
Mathias Wawer <mwawer@broadinstitute.org>
2018-09-20 09:20:10 -0700

    remove comments

--- load_neo4j/load_all.sh
+++ load_neo4j/load_all.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# user=$1
-# password=$2
+user=$1
+password=$2
 
-# cypher-shell -u $user -p $password < create_indexes.cypher
+cypher-shell -u $user -p $password < create_indexes.cypher
 
-# python load_drugs.py
-# python load_genes_proteins.py
-# python load_drug_target_relations.py
-# python load_diseases.py
-# python load_indications.py
-# python load_semmeddb_data.py
+python load_drugs.py
+python load_genes_proteins.py
+python load_drug_target_relations.py
+python load_diseases.py
+python load_indications.py
+python load_semmeddb_data.py
 python load_uberon_ontology.py
 python load_cell_ontology.py
 python load_chebi_ontology.py
